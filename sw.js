a689922320897ee5b17ca51955fd181ed7eb9ab7
levmasenkov-ai <lev.masenkov@gmail.com>
2026-08-08 13:17:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-
+ 
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
